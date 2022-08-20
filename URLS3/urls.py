1712b2a99c2d99dbab857ab23b18a7282dc37e2f
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-20 10:44:14 +0900

    re-initializing.

--- URLS3/urls.py
+++ URLS3/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
-
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path(os.getenv('DJANGO_REAL_ADMIN'), admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
