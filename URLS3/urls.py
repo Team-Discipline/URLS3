5da20b119ebe34bb9c19ea9297dee9260a32a1b4
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-20 12:10:26 +0900

    Implement basic urls.

--- URLS3/urls.py
+++ URLS3/urls.py
@@ -1,8 +1,12 @@
-"""URLS3 URL Configuration
+import os
 
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f"{os.getenv('DJANGO_REAL_ADMIN')}", admin.site.urls),
+    path('docs/', schema_view.with_ui(cache_timeout=0), name='schema-json'),
+    path('token/', include('login.urls')),
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
 ]
