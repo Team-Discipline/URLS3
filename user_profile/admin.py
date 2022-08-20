3199b010622d9671d141dc92d2074769b325b3ac
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-20 19:25:57 +0900

    Implement admin models.

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+
+from user_profile.models import UserProfile, Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'uploaded_by', 'image', 'created_at', 'updated_at']
+    search_fields = ['id', 'uploaded_by']
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'thumbnail', 'created_at', 'updated_at']
+    search_fields = ['id', 'user']
