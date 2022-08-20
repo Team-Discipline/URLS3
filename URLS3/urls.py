c0ddf791620f30d16b0bcbe04c64b6a291d30428
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-20 12:11:00 +0900

    Connect with Swagger documentation.

--- URLS3/urls.py
+++ URLS3/urls.py
@@ -1,7 +1,20 @@
 import os
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="URLS3",
+        default_version='0.1.0',
+        description="Make URL Shortener, Securer, Sexier.",
+    ),
+    public=False,
+    permission_classes=[permissions.IsAuthenticated],
+)
 
 urlpatterns = [
     path(f"{os.getenv('DJANGO_REAL_ADMIN')}", admin.site.urls),
