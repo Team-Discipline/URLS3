ca18b5d6ae18e447da0ec29fa34e093b7cf25146
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-06 01:37:26 +0900

    Implement CapturedData model.

---
+++ analytics/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class CapturedData(models.Model):
+    # TODO: S3 Information.
+    # s3 = models.ForeignKey(S3k, on_delete=models.CASCADE, related_name='captured_data')
+    # User's ip address.
+    ip_address = models.GenericIPAddressField()
+    # When JS collector is first loaded.
+    js_reqeust_time_UTC = models.DateTimeField()
+    # When ads page is fully loaded.
+    page_loaded_time = models.DateTimeField()
+    # When user is trying to leave ads page. `window.onbeforeunload`
+    page_leave_time = models.DateTimeField()
+    # URL before entered ads page.
+    referer_url = models.URLField()
+    # When `this` model is created. This field is created automatically.
+    created_at = models.DateTimeField(auto_now_add=True)
