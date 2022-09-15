1133b095b7e9c42eca03f242fc368b82818a5645
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-15 15:46:47 +0900

    Fix to able to get urls only what I made url short.

--- S3/views.py
+++ S3/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from S3.models import S3
 from S3.serializers import S3Serializer
@@ -11,3 +14,10 @@ class S3ViewSet(viewsets.ModelViewSet):
     queryset = S3.objects.all()
     serializer_class = S3Serializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request: Request, *args, **kwargs):
+        if not request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        self.queryset = S3.objects.filter(issuer=request.user)
+        return super().list(request, *args, **kwargs)
