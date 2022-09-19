cc765f1688005dc02e0053f8022bdc75acf34d0b
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-19 13:42:57 +0900

    Fix jwt error.

--- URLS3/urls.py
+++ URLS3/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path(f"{os.getenv('DJANGO_REAL_ADMIN')}", admin.site.urls),
     path('docs/', schema_view.with_ui(cache_timeout=0), name='schema-json'),
     path('token/', include('login.urls')),
-    path('', include('dj_rest_auth.urls')),
+    path('token/', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('profile/', include('user_profile.urls')),
     path('analytics/', include('analytics.urls')),
