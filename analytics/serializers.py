11fd80a0b799b6bab771ab84873de926984e3446
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-25 02:04:42 +0900

    Fix sth in my flavors.

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 
+from S3.models import S3
 from analytics.models import CapturedData
 
 
-class CreateCapturedDataSerializer(serializers.ModelSerializer):
+class CreateCapturedDataSerializer(serializers.HyperlinkedModelSerializer):
+    s3_s3_url = serializers.HyperlinkedRelatedField(view_name='URLS3:s3-detail',
+                                                    lookup_field='s3_url',
+                                                    queryset=S3.objects.all())
+
     class Meta:
         model = CapturedData
-        exclude = ['id', 'ip_address', 'country', 'city', 'latitude', 'longitude']  # 's3']
+        exclude = ['ip_address', 'country', 'city', 'latitude', 'longitude', 's3']
+        extra_kwargs = {
+            'url': {
+                'view_name': 'captureddata-detail'
+            }
+        }
 
 
-class GetCapturedDataSerializer(serializers.ModelSerializer):
+class GetCapturedDataSerializer(serializers.HyperlinkedModelSerializer):
     ip_address = serializers.IPAddressField()
 
     class Meta:
