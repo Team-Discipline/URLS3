5ef38f4fff43c856e94f8558706ccfabb86551fd
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-25 02:04:07 +0900

    Add new fields to model.

--- analytics/models.py
+++ analytics/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+from S3.models import S3
+
 
 class CapturedData(models.Model):
-    # TODO: S3 Information.
-    # s3 = models.ForeignKey(S3k, on_delete=models.CASCADE, related_name='captured_data')
+    s3 = models.ForeignKey(S3, on_delete=models.CASCADE, related_name='captured_data')
     # User's ip address.
     ip_address = models.GenericIPAddressField()
     # When JS collector is first loaded.
@@ -22,3 +23,6 @@ class CapturedData(models.Model):
     city = models.CharField(max_length=1000, null=True, blank=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.ip_address}'
