a6c81dea330d1240d7e2b73c1d386f37ac34fc38
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-25 19:45:05 +0900

    Fix basename to use HyperlinkedRelatedField of drf.

--- analytics/urls.py
+++ analytics/urls.py
@@ -4,7 +4,7 @@ from analytics.views import AnalyticsViewSet, CollectDataViewSet
 
 router = DefaultRouter()
 
-router.register('collect', CollectDataViewSet, basename='collect_data')
+router.register('collect', CollectDataViewSet, basename='captureddata')
 router.register('', AnalyticsViewSet, basename='analytics')
 
 urlpatterns = router.urls
