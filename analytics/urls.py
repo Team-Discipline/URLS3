dd541c4032554e33763a82d5a55e80ba1e191dda
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-25 23:55:49 +0900

    Implement new uri entrypoint.

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from analytics.views import AnalyticsViewSet
+from analytics.views import AnalyticsViewSet, UniqueVisitorsViewSet
 
 urlpatterns = [
-    path('<int:s3_id>/', AnalyticsViewSet.as_view())
+    path('<int:s3_id>/unique_visitors/', UniqueVisitorsViewSet.as_view()),
+    path('<int:s3_id>/', AnalyticsViewSet.as_view()),
 ]
