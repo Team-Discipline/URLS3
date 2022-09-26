097e5317c8e47da432fe8117a4705d51a89f7657
ryankimjh00 <ryankimjh546@gmail.com>
2022-09-26 12:36:52 +0900

    initial migration

--- urlvalidator/models.py
+++ urlvalidator/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class URLModel(models.Model):
+    objects = None
     url = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
--- urlvalidator/tests.py
+++ urlvalidator/tests.py
@@ -2,16 +2,11 @@ from django.test import TestCase
 from urlvalidator.views import ValidateUrl
 from urlvalidator.models import URLModel
 
+
 # Create your tests here.
 
 
 class URLvalidationTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self) -> None:
-        pass
-
     def test_url_validation(self):
-        url = URLModel.objects.create(url="www.google.com")
+        url = URLModel.objects.create(url="https://www.google.com/?gws_rd=ssl")
         ValidateUrl.is_valid_url(url)
