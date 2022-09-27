829e1be648753a34a84deaf74af6d717d179a805
gyuri <rbfl0616@naver.com>
2022-09-27 12:40:48 +0900

    viewsets

--- access_key/views.py
+++ access_key/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_api_key.permissions import HasAPIKey
 
-# Create your views here.
+from access_key.models import UserAccessKey
+from access_key.serializers import GetUserAccessKeySerializer, CreateUserAccessKeySerializer
+
+
+class AccessKeyViewSet(viewsets.ModelViewSet):
+    permission_classes = [HasAPIKey]
+    queryset = UserAccessKey.objects.all()
+    serializer_class = GetUserAccessKeySerializer
+    http_method_names = ['get']
+
+
+class CreateKeyViewSet(viewsets.ModelViewSet):
+    permission_classes = [HasAPIKey]
+    queryset = UserAccessKey.objects.all()
+    serializer_class = CreateUserAccessKeySerializer
+    http_method_names = ['post']
