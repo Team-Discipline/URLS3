72e12224150887734454925f250fe08120718b85
gyuri <rbfl0616@naver.com>
2022-09-27 12:40:14 +0900

    model

--- access_key/models.py
+++ access_key/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserAccessKey(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    access_key = models.CharField(max_length=1000)
+    expires = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
