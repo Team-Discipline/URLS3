5254de59f54ab8d93c987a7de85efe2f427ecfc3
gyuri <rbfl0616@naver.com>
2022-09-27 12:41:05 +0900

    admin

--- access_key/admin.py
+++ access_key/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from access_key.models import UserAccessKey
+
+
+@admin.register(UserAccessKey)
+class UserAccessKeyAdmin(admin.ModelAdmin):
+    list_filter = ['id', 'user', 'access_key', 'expires', 'created_at']
+    search_fields = list_filter
