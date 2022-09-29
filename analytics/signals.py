ca62491aa8c29c50e6e87e15cbbf924e86decbe4
Aksidion Kreimben <aksidion@kreimben.com>
2022-09-29 12:51:18 +0900

    Fix logical errors.

--- analytics/signals.py
+++ analytics/signals.py
@@ -9,7 +9,12 @@ print('django signal loaded!')
 
 @receiver(post_save, sender=CapturedData)
 def validate_data_and_create_unique_visitors(sender: CapturedData, instance: CapturedData, *args, **kwargs):
-    latest = CapturedData.objects.filter(s3=instance.s3, ip_address=instance.ip_address).order_by('-created_at')[1]
+    history = CapturedData.objects.filter(s3=instance.s3, ip_address=instance.ip_address)
 
-    if latest.created_at + timedelta(minutes=30) < instance.created_at:
+    if history.count() > 1:
+        latest = history.order_by('-created_at')[1]
+
+        if latest.created_at + timedelta(minutes=30) < instance.created_at:
+            UniqueVisitor(data=instance).save()
+    else:
         UniqueVisitor(data=instance).save()
