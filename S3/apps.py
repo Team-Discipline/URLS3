b6d1cde39c9ff5d8ea160fc811686803bf9d3a10
Aksidion Kreimben <aksidion@kreimben.com>
2022-10-30 18:19:06 +0900

    Refactoring.

--- S3/apps.py
+++ S3/apps.py
@@ -6,13 +6,12 @@ class S3Config(AppConfig):
     name = 'S3'
 
     def ready(self):
-        from . import signals
         """
-        밑의 줄을 import 함으로써 `utils.words_excel`안에 있는 엑셀 파일을 불러들여
-        `Word` 모델에 저장합니다.
-
-        평상시에는 주석을 해제하지 마십시오.
+        `utils.words_excel`안에 있는 엑셀 파일을 불러들여 `Word` 모델에 저장합니다.
+        주석 건들지 마시오.
         """
-
-        # from .utils import LoadWords
+        # from .utils.LoadWords import load_adj, load_noun, delete_every_words
+        # delete_every_words()
+        # load_adj()
+        # load_noun()
         ...
