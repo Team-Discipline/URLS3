c34d916b098e6439636ab920fc76c17e5754c221
Aksidion Kreimben <aksidion@kreimben.com>
2022-11-05 16:41:18 +0900

    Add url name for recognizing url.

--- S3/urls.py
+++ S3/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from S3.views import S3CreateGetViewSet, S3UpdateDeleteViewSet
 
 urlpatterns = [
-    path('', S3CreateGetViewSet.as_view()),
-    path('<int:s3_id>/', S3UpdateDeleteViewSet.as_view())
+    path('', S3CreateGetViewSet.as_view(), name='s3'),
+    path('<int:s3_id>/', S3UpdateDeleteViewSet.as_view(), name='s3-delete')
 ]
