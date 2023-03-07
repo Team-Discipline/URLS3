b4499faf49e0a66933b3b4160624551046b3ba2d
Aksidion Kreimben <aksidion@kreimben.com>
2023-03-07 12:32:02 +0900

    Fix consumer logic.

--- S3/consumers.py
+++ S3/consumers.py
@@ -2,9 +2,7 @@ import asyncio
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-from S3.models import Hash, S3
-from analytics.models import CapturedData
-from analytics.serializers import GetCapturedDataSerializer
+from S3.models import S3
 
 
 class AdPageConsumer(AsyncJsonWebsocketConsumer):
@@ -15,28 +13,23 @@ class AdPageConsumer(AsyncJsonWebsocketConsumer):
 
             try:
                 # When only protocol is fit and hash value is valid.
-                await Hash.objects.aget(hash_value=hashed_value)
-                return await self.accept()
-            except Hash.DoesNotExist:
-                return
-        else:
-            return
-
-    async def receive_json(self, content, **kwargs):
-        if content.get('captured_data', None):
-            captured_data_id = content['captured_data']
-            try:
-                c = await CapturedData.objects.aget(id=captured_data_id)
-                s = GetCapturedDataSerializer(c)
-                await self.send_json({'message': 'data checked', 'content': s.data})
-                await self.wait_for_ads()
-                s3 = await S3.objects.aget(id=s.data.get('s3'))
+                s3 = await S3.objects.aget(hashed_value__hash_value=hashed_value)
+                await self.accept()
+                await asyncio.sleep(3)
                 return await self.send_json({'success': True,
-                                             'message': 'go to target_url!',
+                                             'msg': 'go to target_url!',
                                              'target_url': s3.target_url},
                                             close=True)
-            except CapturedData.DoesNotExist:
-                return await self.send_json({'success': False, 'message': 'cannot find data.'})
+            except S3.DoesNotExist:
+                await self.send_json({
+                    'success': False,
+                    'msg': '해당되는 S3 데이터를 찾을 수 없습니다.'
+                }, close=True)
+        else:
+            await self.send_json({
+                'success': False,
+                'msg': '해시값이 필요합니다.'
+            }, close=True)
 
     async def wait_for_ads(self, second: int = 5):
         return await asyncio.sleep(second)
